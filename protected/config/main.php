1707e2d79da65153a872a15e3e0d8ba6ac9ab42f
Lion <Lion@Lion-PC>
2016-08-25 20:16:06 +0800

    Signed-off-by: Lion <Lion@Lion-PC>

--- protected/config/main.php
+++ protected/config/main.php
@@ -2,12 +2,13 @@
 
 // uncomment the following to define a path alias
 // Yii::setPathOfAlias('local','path/to/local-folder');
+Yii::setPathOfAlias('blog', Yii::getFrameworkPath() . '/../demos/blog');
 
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name' => 'ITSC',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -17,6 +18,7 @@ return array(
 		'application.models.*',
 		'application.components.*',
 		'application.helpers.*',
+		'blog.protected.components.UserIdentity',
 	),
 
 	'modules'=>array(
--- protected/models/ImageFile.php
+++ protected/models/ImageFile.php
@@ -1,21 +1,6 @@
 <?php
 
 class ImageFile extends BaseImage {
-/*	public function insert($attributes = null) {
-		$attributes = $this->getAttributes($attributes);
-		$created_at = date('Y-m-d h:i:s');
-		$sql = "INSERT INTO t_image (id, path, created_at) VALUES(replace(uuid(), '-', ''), :path, :created_at)";
-		$connection = Yii::app()->db;
-		$command = $connection->createCommand($sql);
-		$command->bindParam(":path", $attributes['path'], PDO::PARAM_STR);
-		$command->bindParam(":created_at", $created_at, PDO::PARAM_STR);
-		$command->execute();
-		
-		$image = ImageFile::model(__CLASS__)->findByAttributes(array('path' => $attributes['path'], 'created_at' => $created_at));
-		$this->id = $image->id;
-		return true;
-	}
-*/	
 	protected function beforeSave() {
 		if (parent::beforeSave()) {
 			if ($this->isNewRecord) {
